e56be59de0623dae0301787efd0ef5dfdf2c37f3
jsofiah <j.sofiah18@gmail.com>
2025-11-23 09:23:03 +0700

    revisi artikel

--- artikel.php
+++ artikel.php
@@ -135,7 +135,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styleRoot.css">
     <link rel="stylesheet" href="css/styleArtikel.css">
-    <link rel="stylesheet" href="css/styleIndex.css">
     <link rel="stylesheet" href="css/styleFooter.css">
 
 </head>
@@ -182,7 +181,7 @@
     <div class="hero-wrapper">
         <div class="hero-container">
             <div class="hero-frame">
-                <img src="img/background_index.jpg" alt="Lab Background">
+                <img src="img/bgartikel.jpg" alt="Lab Background">
                 <div class="hero-overlay"></div>
                 <div class="hero-content">
                 <h1 class="hero-title">ARTIKEL
