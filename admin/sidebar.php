26724e7ce1a65006a6b928eae4274e6ed7b77ba9
jsofiah <j.sofiah18@gmail.com>
2025-11-25 13:47:07 +0700

    revisi sidebar

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -46,8 +46,7 @@ $initial = strtoupper(substr($namaAdmin, 0, 2));
                     <i class="fas fa-chevron-right chevron"></i>
                 </div>
                 <div class="submenu" id="profil">
-                    <a href="kelola_tentang.php" class="submenu-link"><i class="fas fa-info-circle"></i> Tentang Lab</a>
-                    <a href="kelola_visimisi.php" class="submenu-link"><i class="fas fa-bullseye"></i> Visi Misi</a>
+                    <a href="kelola_konten.php" class="submenu-link"><i class="fas fa-info-circle"></i> Konten Lab</a>
                     <a href="kelola_roadmap.php" class="submenu-link"><i class="fas fa-road"></i> Roadmap</a>
                     <a href="kelola_fasilitas.php" class="submenu-link"><i class="fas fa-door-open"></i> Fasilitas</a>
                     <a href="kelola_mitra.php" class="submenu-link"><i class="fas fa-handshake"></i> Mitra</a>
