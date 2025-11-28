ebd4f18f6f665a9d69e5531c9e279cc7fb6bfa1a
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2025-11-28 22:24:04 +0700

    update CRUD artikel

--- admin/kelola_artikel.php
+++ admin/kelola_artikel.php
@@ -16,13 +16,13 @@ $rArtikel = pg_query($conn, $qArtikel);
 <html lang="id">
 <head>
 <meta charset="UTF-8">
-<title>Kelola Artikel</title>
+    <title>Kelola Artikel</title>
 
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-
-<link rel="stylesheet" href="css/styleSidebar.css">
-<link rel="stylesheet" href="css/styleTabel.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styleSidebar.css">
+    <link rel="stylesheet" href="css/styleTabel.css">
 </head>
 
 <body>
