dc1b38c64fc9569c2796ffa2d0b53b85af7f38aa
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2025-11-28 22:19:48 +0700

    update CRUD jenis publikasi

--- admin/kelola_jenisPublikasi.php
+++ admin/kelola_jenisPublikasi.php
@@ -17,9 +17,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kelola Jenis Publikasi</title>
-
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styleSidebar.css">
     <link rel="stylesheet" href="css/styleTabel.css">
 </head>
