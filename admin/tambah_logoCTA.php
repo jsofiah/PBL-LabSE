08af32a01dd5f4d1a0dd776a76d6120e2d1be858
unknown <bima.rafi60@sma.belajar.id>
2025-12-01 20:07:50 +0700

    update logoCTA

--- admin/tambah_logoCTA.php
+++ admin/tambah_logoCTA.php
@@ -9,23 +9,24 @@ require "../config.php";
 
 if (isset($_POST['simpan'])) {
 
-    $logoBaru = "";
+    $logo = "";
 
     if (!empty($_FILES['logo']['name'])) {
-        $targetDir = "../img/logocta/";
-        if (!is_dir($targetDir)) mkdir($targetDir);
+        $dir = "../img/logocta/";
+        if (!is_dir($dir)) mkdir($dir);
 
-        $filename = time() . "_" . basename($_FILES["logo"]["name"]);
-        $targetFile = $targetDir . $filename;
+        $filename = time() . "_" . basename($_FILES['logo']['name']);
+        $target = $dir . $filename;
 
-        if (move_uploaded_file($_FILES["logo"]["tmp_name"], $targetFile)) {
-            $logoBaru = "img/logocta/" . $filename;
+        if (move_uploaded_file($_FILES['logo']['tmp_name'], $target)) {
+            $logo = "img/logocta/" . $filename;
         }
     }
 
-    $query = "CALL sp_create_logocta('$_POST[judul]', '$_POST[link]', '$logoBaru');";
+    $q = "CALL sp_create_logocta($1, $2, $3)";
+    $params = array($_POST['judul'], $_POST['link'], $logo);
 
-    pg_query($conn, $query);
+    pg_query_params($conn, $q, $params);
 
     echo "<script>alert('Logo CTA berhasil ditambahkan!'); window.location='kelola_logoCTA.php';</script>";
     exit;
@@ -33,42 +34,53 @@ if (isset($_POST['simpan'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang='id'>
+<html lang="id">
 <head>
-<meta charset='UTF-8'>
+<meta charset="UTF-8">
 <title>Tambah Logo CTA</title>
-<link href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css' rel='stylesheet'>
-<link rel='stylesheet' href='css/styleForm.css'>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/styleForm.css">
+<link rel="stylesheet" href="css/styleSidebar.css">
 </head>
 
-<body class='p-4'>
-<div class='container'>
-    <h1 class='mb-4 fw-bold text-center'>Tambah Logo CTA</h1>
+<body>
 
-    <div class='card shadow-sm p-4'>
-        <form method='POST' enctype='multipart/form-data'>
+<?php include 'sidebar.php'; ?>
 
-            <div class='mb-3'>
-                <label class='form-label text-white'>Upload Logo</label>
-                <input type='file' name='logo' class='form-control' required>
+<div class="content-area container">
+    <h1 class="mb-4 fw-bold text-center">Tambah Logo CTA</h1>
+
+    <div class="card shadow-sm p-4 form-card">
+
+        <form method="POST" enctype="multipart/form-data">
+
+            <div class="mb-3">
+                <label class="form-label text-white">Upload Logo</label>
+                <input type="file" name="logo" class="form-control" required>
             </div>
 
-            <div class='mb-3'>
-                <label class='form-label text-white'>Judul CTA</label>
-                <input type='text' name='judul' class='form-control' required>
+            <div class="mb-3">
+                <label class="form-label text-white">Judul CTA</label>
+                <input type="text" name="judul" class="form-control" 
+                placeholder="Masukkan judul logo cta" required>
             </div>
 
-            <div class='mb-3'>
-                <label class='form-label text-white'>Link CTA</label>
-                <input type='text' name='link' class='form-control' required>
+            <div class="mb-3">
+                <label class="form-label text-white">Link CTA</label>
+                <input type="text" name="link" class="form-control" 
+                placeholder="Masukkan link logo cta" required>
             </div>
 
-            <button type='submit' name='simpan' class='btn btn-primary mt-3'>
-                <i class='fa fa-save'></i> Simpan
-            </button>
-            <a href='kelola_logoCTA.php' class='btn btn-secondary mt-3'>Kembali</a>
+            <div class="d-flex gap-2 mt-3">
+                <button type="submit" name="simpan" class="btn btn-success">Simpan</button>
+                <a href="kelola_logoCTA.php" class="btn btn-secondary">Kembali</a>
+            </div>
+
         </form>
+
     </div>
 </div>
+
+<script src="js/sidebar.js"></script>
 </body>
 </html>
