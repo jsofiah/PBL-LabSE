cf2808ef1ff000367ebb6e3f8b50199a27e6d15c
unknown <bima.rafi60@sma.belajar.id>
2025-12-02 10:20:28 +0700

    update proyek_dosen

--- admin/hapus_proyek_dosen.php
+++ admin/hapus_proyek_dosen.php
@@ -4,13 +4,20 @@ if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit;
 }
+
 require "../config.php";
 
-$id_proyek = $_GET['id_proyek'];
-$id_dosen  = $_GET['id_dosen'];
+$id_proyek = intval($_GET['id_proyek']);
+$id_dosen  = intval($_GET['id_dosen']);
 
-pg_query($conn, "CALL sp_delete_proyek_dosen($id_proyek, $id_dosen)");
+$query = "CALL sp_delete_proyek_dosen($1, $2)";
+$params = array($id_proyek, $id_dosen);
 
-echo "<script>alert('Relasi berhasil dihapus!'); window.location='kelola_proyek_dosen.php';</script>";
+pg_query_params($conn, $query, $params);
+
+echo "<script>
+        alert('Relasi proyek–dosen berhasil dihapus!');
+        window.location='kelola_proyek.php';
+      </script>";
 exit;
 ?>
