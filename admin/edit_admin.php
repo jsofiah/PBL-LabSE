f2cec49513401aaefd79ce6ea360e887ff7ab7db
unknown <rifatsyamlan78@gmail.com>
2025-12-03 23:43:11 +0700

    revisi sidebar edit admin

--- admin/edit_admin.php
+++ admin/edit_admin.php
@@ -74,9 +74,13 @@ if (isset($_POST['update'])) {
     <title>Edit Admin - Portal LAB SE</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styleSidebar.css">
     <link rel="stylesheet" href="css/styleForm.css">
 </head>
 <body class="p-4">
+    <?php include 'sidebar.php'; ?>
+
     <div class="content-area container">
         <h1 class="mb-4 fw-bold text-center">Edit Data Admin</h1>
         <?php if(isset($error)) echo "<div class='alert alert-danger'>$error</div>"; ?>
@@ -106,5 +110,6 @@ if (isset($_POST['update'])) {
             </div>
         </form>
     </div>
+    <script src="js/sidebar.js"></script>
 </body>
 </html>
