6c48d246524bcb85bda7f197ee877a1b32205213
tersiqo <tersiqo.alfarezel@gmail.com>
2025-12-05 16:15:06 +0700

    revisi tombol footer

--- admin/edit_footer.php
+++ admin/edit_footer.php
@@ -163,11 +163,9 @@
                 </div>
 
                 <div class="d-flex gap-2 mt-3">
-                    <button type="submit" name="update" class="btn btn-primary">
-                        <i class="fa fa-save"></i> Simpan Perubahan
+                    <button type="submit" name="update" class="btn btn-primary">Simpan Perubahan
                     </button>
-                    <a href="kelola_footer.php" class="btn btn-secondary">
-                        <i class="fa fa-arrow-left"></i> Kembali
+                    <a href="kelola_footer.php" class="btn btn-secondary">Kembali
                     </a>
                 </div>
             </form>
